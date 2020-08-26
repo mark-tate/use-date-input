7e972cdedafc9e9f81ae1911f53fed36751c91a4
Mark <143323+mark-tate@users.noreply.github.com>
2020-08-26 14:26:50 +0200

    docs: add richer example of composition using Material UI

---
+++ src/gatsby-theme-docz/components/DemoDropdown.js
@@ -0,0 +1,62 @@
+import {
+  FormControl,
+  Input,
+  InputLabel,
+  MenuItem,
+  Select,
+  makeStyles
+} from "@material-ui/core";
+import PropTypes from "prop-types";
+import React from "react";
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120
+  }
+}));
+
+export default function Dropdown({
+  label,
+  labelId,
+  onChange,
+  selectedValue,
+  source
+}) {
+  const classes = useStyles();
+  const handleChange = event => onChange(event.target.value);
+
+  return (
+    <FormControl className={classes.formControl}>
+      <InputLabel id={labelId}>{label}</InputLabel>
+      <Select
+        labelId={labelId}
+        value={selectedValue}
+        onChange={handleChange}
+        input={<Input />}
+      >
+        {source.reduce(
+          (accumulator, item) => [
+            ...accumulator,
+            <MenuItem key={item.key} value={item.value}>
+              {item.label}
+            </MenuItem>
+          ],
+          []
+        )}
+      </Select>
+    </FormControl>
+  );
+}
+
+Dropdown.propTypes = {
+  labelId: PropTypes.string,
+  label: PropTypes.string,
+  onChange: PropTypes.func,
+  selectedValue: PropTypes.string,
+  source: PropTypes.array
+};
